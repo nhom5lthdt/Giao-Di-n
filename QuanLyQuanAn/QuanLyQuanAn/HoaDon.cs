a518ae5335121b6bfe4404dc6b3f55a43daaf199
Nguyễn Khánh Đức <khanhduc1998123@gmail.com>
2017-11-28 11:22:09 +0700

    Them Phan Bao Cao

---
+++ QuanLyQuanAn/QuanLyQuanAn/HoaDon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanAn
+{
+    public partial class HoaDon : Form
+    {
+        public HoaDon()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cnn = new SqlConnection(@"Data Source= UOL;Initial Catalog=QuanLyQuanAn;Integrated Security=True");
+        private void ketnoicsdl()
+        {
+            cnn.Open();
+            string sql = "select * from BaoCao";  // lay het du lieu trong bang sinh vien
+            SqlCommand com = new SqlCommand(sql, cnn); //bat dau truy van
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com); //chuyen du lieu ve
+            DataTable dt = new DataTable(); //tạo một kho ảo để lưu trữ dữ liệu
+            da.Fill(dt);  // đổ dữ liệu vào kho
+            cnn.Close();  // đóng kết nối
+            dataGridView1.DataSource = dt; //đổ dữ liệu vào datagridview
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
